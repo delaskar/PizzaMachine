2966305c48331e97182eb296d66fa0b5cb2d60e8
Delaskar Caicedo <delaskarcai@gmail.com>
2023-02-01 12:35:52 +0200

    Created the list table of categories

--- presetpizza.py
+++ presetpizza.py
@@ -1,5 +1,31 @@
 from menu import MENU
-
+from prettytable import PrettyTable
 
 # Presetpizza
+class Presetpizzas:
 
+    def __init__(self, category_letter):
+        self.category_letter = category_letter
+        self.category_names = []
+        self.pizza_names = []
+        self.ingredients = []
+        self.price = []
+
+
+    def listPresetCategories(self):
+        """This method return the list of preset categories"""
+        
+        table = PrettyTable()
+        table.field_names = ["Letter", "Categories"]        
+
+        for key1, value1 in MENU.items():
+            for key2, value2 in value1.items():
+                table.add_row([key1, key2])
+        
+        print(table)
+
+
+if __name__ == "__main__":
+    oPresetPizza = Presetpizzas("test")
+    oPresetPizza.listPresetCategories()
+    print("This is the class Presetpizza")
