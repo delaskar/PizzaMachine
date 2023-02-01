5291884ddfe3eb89bacac3433d9b8eef0863cedf
Delaskar Caicedo <delaskarcai@gmail.com>
2023-02-01 10:42:54 +0200

    Create a default project with 5 files: .gitignore, main, menu, pressetpizza, requirements

---
+++ menu.py
@@ -0,0 +1,395 @@
+MENU = {
+    "a": {
+        "HITS": {
+            1: {
+                "NEW G": {
+                    "Ingredients": [
+                        "classic tomato",
+                        "fresh mozzarella",
+                        "special T sauce",
+                        "basil"
+                    ], "price": 50.0
+                }
+            },
+            2: {
+                "BUFFALO 66": {
+                    "Ingredients": [
+                        "spicy tomato fresh"
+                        "mozzarella",
+                        "special T sauce",
+                        "pepperoni",
+                        "pickled fresno chilies",
+                        "red pepper flakes"
+                    ], "price": 65.0
+                }
+            },
+            3: {
+                "DIRTY BIRD": {
+                    "Ingredients": [
+                        "parm-romano sauce",
+                        "shredded mozzarella",
+                        "chicken",
+                        "jalapeno pepper",
+                        "buffalo sauce",
+                        "ranch dressing",
+                        "crumbled blue cheese"
+                    ], "price": 70.0
+                }
+            },
+            4: {
+                "MAVERICK": {
+                    "Ingredients": [
+                        "classic tomato",
+                        "fresh mozzarella",
+                        "salami",
+                        "pepperoni",
+                        "italian sausage",
+                        "basil pesto",
+                        "parmesan-romano"
+                    ], "price": 65.0
+                }
+            },
+            5: {
+                "AMERICAN HONEY": {
+                    "Ingredients": [
+                        "spicy tomato",
+                        "fresh mozzarella",
+                        "pepperoni",
+                        "arugula",
+                        "goat cheese",
+                        "hot honey",
+                        "red pepper flakes"
+                    ], "price": 70.5
+                }
+            },
+        }
+    },
+    "b": {
+        "HEAVY HITTERS": {
+            1: {
+                "THE BILLIE": {
+                    "Ingredients": [
+                        "PARM-ROMANO SAUCE",
+                        "FRESH MOZZARELLA",
+                        "GRILLED ONION",
+                        "PARMESAN-ROMANO",
+                        "BASIL PESTO"
+                    ], "price": 66.0
+                }
+            },
+            2: {
+                "KAMALA HARRIS": {
+                    "Ingredients": [
+                        "SPINACH",
+                        "GARLIC PUREE",
+                        "ROASTED RED PEPPERS",
+                        "GRILLED ONION",
+                        "KAMALA OLIVES",
+                        "FETA",
+                        "FIG BALSAMIC"
+                    ], "price": 78.3
+                }
+            },
+            3: {
+                "THEE OUTLAW": {
+                    "Ingredients": [
+                        "PARM-ROMANO SAUCE",
+                        "SHREDDED MOZZARELLA",
+                        "GRILLED ONION",
+                        "BACON",
+                        "CHICKEN",
+                        "BBQ SAUCE"
+                    ], "price": 90.5
+                }
+            },
+            4: {
+                "PINEAPPLE JACK'D": {
+                    "Ingredients": [
+                        "CLASSIC TOMATO",
+                        "SHREDDED MOZZARELLA",
+                        "PINEAPPLE",
+                        "JALAPENO PEPPERS",
+                        "BACON",
+                        "HOT HONEY"
+                    ], "price": 55.0
+                }
+            },
+        }
+    },
+    "c": {
+        "ROLLING STONED": {
+            1: {
+                "KNAUGHTY KNOTS": {
+                    "Ingredients": [
+                        "SWEET RICOTTA",
+                        "CHOCOLATE SAUCE DRIZZLE",
+                        "VANILLA ICING DRIZZLE",
+                        "POWDERED SUGAR"
+                    ], "price": 66.0
+                }
+            },
+            2: {
+                "VEGAN KNOTS": {
+                    "Ingredients": [
+                        "GARLIC PUREE",
+                        "VEGAN MOZZARELLA",
+                        "VEGAN SAUSAGE CRUMBLE",
+                        "GRILLED ONION",
+                        "THE BLEND",
+                        "GARLIC PUREE DRIZZLE",
+                        "CLASSIC TOMATO DIPPING SAUCE"
+                    ], "price": 80.0
+                }
+            },
+            3: {
+                "MOZZ KNOTS": {
+                    "Ingredients": [
+                        "PARMESAN-ROMANO SAUCE",
+                        "PARMESAN-ROMANO",
+                        "SHREDDED MOZZARELLA",
+                        "GARLIC PUREE",
+                        "THE BLEND",
+                        "GARLIC PUREE DRIZZLE",
+                        "GRATED PARMESAN-ROMANO",
+                        "CLASSIC TOMATO DIPPING SAUCE"
+                    ], "price": 75.0
+                }
+            },
+            4: {
+                "HOT KNOTS": {
+                    "Ingredients": [
+                        "PARMESAN-ROMANO SAUCE",
+                        "PARMESAN-ROMANO",
+                        "GARLIC PUREE",
+                        "SHREDDED MOZZARELLA",
+                        "BACON",
+                        "JALAPENO PEPPERS",
+                        "HOT HONEY",
+                        "THE BLEND",
+                        "CLASSIC TOMATO DIPPING SAUCE"
+                    ], "price": 100.0
+                }
+            },
+            5: {
+                "CBR": {
+                    "Ingredients": [
+                        "PARM -ROMANO SAUCE",
+                        "SHREDDED MOZZARELLA",
+                        "PICKLED FRESNO CHILIES",
+                        "GRILLED ONION",
+                        "CHICKEN",
+                        "BACON",
+                        "RANCH"
+                    ], "price": 85.4
+                }
+            },
+            6: {
+                "BUFFY": {
+                    "Ingredients": [
+                        "CLASSIC TOMATO",
+                        "SPECIAL T SAUCE",
+                        "SHREDDED MOZZARELLA",
+                        "GRILLED ONION",
+                        "THE BLEND",
+                        "CRACKED BLACK PEPPER",
+                        "GARLIC PUREE"
+                    ], "price": 95.0
+                }
+            },
+            7: {
+                "@ME DON'T SUB ME": {
+                    "Ingredients": [
+                        "CLASSIC TOMATO",
+                        "SHREDDED MOZZARELLA",
+                        "MUSHROOM",
+                        "GRILLED ONION",
+                        "MEATBALL",
+                        "BANANA PEPPERS",
+                        "PARMESAN-ROMANO"
+                    ], "price": 75.0
+                }
+            }
+        }
+    },
+    "d": {
+        "CHEESE STEEZ": {
+            1: {
+                "BAD AND BLUEZY": {
+                    "Ingredients": [
+                        "SPICY TOMATO SAUCE",
+                        "SHREDDED MOZZARELLA",
+                        "GRILLED ONION",
+                        "PEPPERONI",
+                        "BLUE CHEESE"
+                    ], "price": 60.0
+                }
+            },
+            2: {
+                "THE GOAT": {
+                    "Ingredients": [
+                        "PARM-ROMANO SAUCE",
+                        "SPECIAL T SAUCE",
+                        "FRESH MOZZARELLA",
+                        "GOAT CHEESE",
+                        "FIG BALSAMIC"
+                    ], "price": 66.0
+                }
+            },
+            3: {
+                "HYFR": {
+                    "Ingredients": [
+                        "CLASSIC SAUCE",
+                        "SPECIAL T SAUCE",
+                        "SHREDDED MOZZARELLA",
+                        "PEPPERONI",
+                        "RANCH"
+                    ], "price": 55.0
+                }
+            },
+            4: {
+                "SPICY FIRE": {
+                    "Ingredients": [
+                        "SPICY TOMATO SAUCE",
+                        "SHREDDED MOZZARELLA",
+                        "PICKLED FRESNO CHILIES",
+                        "GRILLED ONIONS",
+                        "ITALIAN SAUSAGE",
+                        "HOT HONEY"
+                    ], "price": 66.0
+                }
+            },
+            5: {
+                "SIMP": {
+                    "Ingredients": [
+                        "CLASSIC SAUCE",
+                        "SPECIAL T SAUCE",
+                        "FRESH MOZZARELLA",
+                        "SHREDDED MOZZARELLA",
+                        "PARMESAN-ROMANO"
+                    ], "price": 55.0
+                }
+            }
+        }
+    },
+    "e": {
+        "BASIC": {
+            1: {
+                "3PEAT": {
+                    "Ingredients": [
+                        "CLASSIC SAUCE",
+                        "SHREDDED MOZZARELLA",
+                        "PEPPERONI",
+                        "ITALIAN SAUSAGE",
+                        "BACON"
+                    ], "price": 55.0
+                }
+            },
+            2: {
+            "MEAT-LIKER'S": {
+                "Ingredients": [
+                    "CLASSIC SAUCE",
+                    "SHREDDED MOZZARELLA",
+                    "PEPPERONI",
+                    "ITALIAN SAUSAGE"
+                ], "price": 55.0
+            }
+            },
+            3: {
+                "GIANT ASS BREADS TICK (G.A.B.S.)": {
+                    "Ingredients": [
+                        "PARM-ROMANO SAUCE",
+                        "SHREDDED MOZZARELLA",
+                        "CRACKED BLACK PEPPER"
+                    ], "price": 45.0
+                }
+            },
+            4: {
+                "LIL RONI": {
+                    "Ingredients": [
+                        "CLASSIC SAUCE",
+                        "SHREDDED MOZZARELLA",
+                        "PEPPERONI"
+                    ], "price": 45.0
+                }
+            },
+            5: {
+                "TENDERONI": {
+                    "Ingredients": [
+                        "PARM-ROMANO SAUCE",
+                        "SHREDDED MOZZARELLA",
+                        "PEPPERONI"
+                    ], "price": 46.5
+                }
+            }
+        }
+    },
+    "f": {
+        "LIMITED EDITION": {
+            1: {
+                "BLK": {
+                    "Ingredients": [
+                        "CLASSIC TOMATO SAUCE",
+                        "BASIL",
+                        "FRESH MOZZARELLA",
+                        "GRILLED ONION",
+                        "CHICKEN",
+                        "CRUSHED RED PEPPER",
+                        "CRACKED BLACK PEPPER",
+                        "GARLIC PUREE"
+                    ], "price": 75.0
+                }
+            },
+        }
+    }
+}
+
+INGREDIENTS = {
+    1: {
+        "DOUGH": [
+            "GLUTEN FREE DOUGH",
+            "TRADITIONAL DOUGH"
+        ]
+    },
+    2: {
+        "SAUCE": [
+            "PARM ROMANO",
+            "BASIL PESTO",
+            "SPICY TOMATO",
+            "CLASSIC TOMATO"
+        ]
+    },
+    3: {
+        "CHEESE": [
+            "FETA CHEESE",
+            "VEGAN MOZZARELLA",
+            "SHREDDED MOZZARELLA",
+            "FRESH MOZZARELLA"
+        ]
+    },
+    4: {
+        "VEGGIES": [
+            "FRESNO PEPPER",
+            "SPINACH",
+            "SPICY CHICKPEA",
+            "ROASTED RED PEPPER",
+            "PINEAPPLE",
+            "MUSHROOM",
+            "JALAPENO",
+            "GRILLED ONION",
+            "GRAPE TOMATO",
+            "BROCCOLI"
+        ]
+    },
+    5: {
+        "PROTEINS": [
+            "VEGAN SAUSAGE",
+            "SHRIMP",
+            "SALAMI",
+            "PEPPERONI",
+            "ITALIAN SAUSAGE",
+            "CHICKEN",
+            "BEEF MEATBALL",
+            "BACON"
+        ]
+    }
+}
---
+++ presetpizza.py
@@ -0,0 +1,5 @@
+from menu import MENU
+
+
+# Presetpizza
+
