3cc78397e54d35eb2ce83dd2d99d68669ef3dc4d
Delaskar Caicedo <delaskarcai@gmail.com>
2023-02-05 15:21:53 +0200

    Remove the while loops and I'm splitting the code to make it more readable, create a third method called selectToppings

--- custompizza.py
+++ custompizza.py
@@ -13,14 +13,9 @@ class Custompizza:
     def __init__(self):
         self.size = [6, 8]
         self.category_numbers = []
-        self.add_topping_counter = 0
-        self.maximum_number_additions = 4
         self.number_toppings_selected = []  # Store the options of toppings
-        self.toppings_selected = []  # Store the name of toppings
-        self.number_of_order = 0
-        self.maximun_order = 0
-        self.counter_order = 0
-        self.total = 0
+        self.topping_names = []  # Store the name of toppings
+        self.topping_selected = []
 
 
     def showToppings(self):
@@ -39,100 +34,79 @@ class Custompizza:
         print(oTcatgory)
 
 
-    def toppings(self):
+    def availableToppings(self):
+
+        clear_cmd = 'os.system("clear")'
+       
+        # Create a available toppings table
+        oTtoppings = PrettyTable()
+        oTtoppings.field_names = ["Name Category", "Number", "Available Toppings"]
+        
+        # Check if the user input is a number from the Toppings category
+        while True:
+            user_topping = input("Enter the number of the category you want to see: ")
+            if user_topping.isdigit():
+                exec(clear_cmd)
+                break
+            else:
+                print("Selection must be a number from category. Try again")
+
+        # converting the user input to an integer
+        user_topping = int(user_topping)
+
+        # List the toppings that are within the categories
+        if user_topping in self.category_numbers:
+            for key1, value1 in INGREDIENTS[user_topping].items():
+                for num, item in enumerate(value1):
+                    # Store the number of topping ↓↓↓↓
+                    self.number_toppings_selected.append(num + 1)
+                    # With this data I make the table ↓↓↓↓
+                    oTtoppings.add_row([key1, num + 1, item.title()])
+                    self.topping_names.append(item.title())
+
+            print(oTtoppings)
+
+        else:
+            print("WRONG TYPING. TRY AGAIN.")
+
+
+        
+        
+    def selectTopping(self):
 
         clear_cmd = 'os.system("clear")'
 
-        while True:
-            user_order = input("How many pizza orders do you want?: ")
-            if user_order.isdigit():
-                break
+        back_off = input("Do you want to select an option? Press 'y' or 'n': ").lower()
+        if back_off == 'n':
+            pass
+            # exec(clear_cmd)
+        elif back_off == 'y':
+            number_selection = int(input("Write the number of your choice: "))
+            # exec(clear_cmd)
+            if number_selection in self.number_toppings_selected:
+                index_selected = number_selection - 1
+                selection = self.topping_names[index_selected]
+                self.topping_selected.append(selection)
+
+                print('')
+                print("Topping Selected ===> ", ', ' .join(self.topping_selected))
+                print('')
             else:
-                print("You must enter an integer. Try again")
-        
-        user_order = int(user_order)
-        self.maximun_order = user_order
-       
-        while True:
+                print('')
+                print("WRONG TYPING. TRY AGAIN.")
+        else:
+            print('')
+            print("WRONG TYPING. TRY AGAIN.")
 
-            if self.counter_order < self.maximun_order:
-                # Call showToppingd
-                self.showToppings()
-            
-                # Create a available toppings table
-                oTtoppings = PrettyTable()
-                oTtoppings.field_names = ["Name Category", "Number", "Available Toppings"]
-                
-                # Check if the user input is a number from the Toppings category
-                while True:
-                    user_topping = input("Enter the number of the category you want to see: ")
-                    if user_topping.isdigit():
-                        exec(clear_cmd)
-                        break
-                    else:
-                        print("Selection must be a number from category. Try again")
-
-                # converting the user input to an integer
-                user_topping = int(user_topping)
-                selections = []
-                if user_topping in self.category_numbers:
-                    for key1, value1 in INGREDIENTS[user_topping].items():
-                        for num, item in enumerate(value1):
-                            self.number_toppings_selected.append(num + 1)
-                            oTtoppings.add_row([key1, num + 1, item.title()])
-                            selections.append(item.title())
-                else:
-                    print("WRONG TYPING. TRY AGAIN.")
-                    break
-
-                print(oTtoppings)
-                
-                is_On = True
-                while is_On:
-                    if self.add_topping_counter < self.maximum_number_additions:
-                        back_off = input("Do you want to select an option? Press 'y' or 'n': ").lower()
-                        if back_off == 'n':
-                            exec(clear_cmd)
-                            is_On = False
-                        elif back_off == 'y':
-                            number_selection = int(input("Write the number of your choice: "))
-                            exec(clear_cmd)
-                            if number_selection in self.number_toppings_selected:
-                                index_selected = number_selection - 1
-                                self.toppings_selected.append(selections[index_selected])
-                                self.add_topping_counter += 1
-                                print('')
-                                print(f"The maximum number of toppings you can choose is: {self.maximum_number_additions * self.maximun_order}, four(4) for each pizza")
-                                print("Number of toppings selected ===> ", self.add_topping_counter)
-                                print("Topping Selected ===> ", ', ' .join(self.toppings_selected))
-                                print('')
-                                is_On = False
-                            else:
-                                print('')
-                                print("WRONG TYPING. TRY AGAIN.")
-                                break
-                        else:
-                            print('')
-                            print("WRONG TYPING. TRY AGAIN.")
-                            break
-
-                if self.add_topping_counter == self.maximum_number_additions:
-                    self.counter_order += 1
-                    print('')
-                    print(f"Your number order is: {self.maximun_order}")
-                    print(f"Your current order is: {self.counter_order}")
-                    print('')
-                else:
-                    pass
-
-            else:
-                for item in range(0, self.maximun_order):
-                    print(f"processing your order {item + 1}...")
-                break
 
 
 
 if __name__ == "__main__":
 
+    # Create Object from the class Custompizza
     oCustompizza = Custompizza()
-    oCustompizza.toppings()
+
+    # Call methods from the oCustompizza
+    oCustompizza.showToppings()
+    oCustompizza.availableToppings()
+    oCustompizza.selectTopping()
